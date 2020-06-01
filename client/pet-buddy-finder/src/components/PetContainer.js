9a65a617b2223774ae2b858463dffa213dda56ed
unknown <lovinmathew@hotmail.com>
2020-06-01 10:38:17 -0500

    new token fix

--- client/pet-buddy-finder/src/components/PetContainer.js
+++ client/pet-buddy-finder/src/components/PetContainer.js
@@ -3,47 +3,65 @@ import PetDetail from "./PetDetail";
 import axios from "axios";
 
 class PetContainer extends Component {
-    state = {
-      results: [],
-      search: ""
-    };
-  
-    // When this component mounts
-    componentDidMount() {
-      //generate token from Postman
-      const token="";
+  state = {
+    results: [],
+    search: ""
+  };
 
-      //localhost is 3000 by default
-      axios.defaults.baseURL = "http://localhost:9000/";
-
-      
-      axios.defaults.headers.common = {'Authorization': `Bearer ${token}`};
-      axios.get("https://api.petfinder.com/v2/animals")
-      .then(res => { 
-        console.log(res)
-        const results = res.data.animals;
-        this.setState({ results});
-      })
-      .catch(err => console.log(err));
-    }
-
-      render() {
-          return (
-              <div>
-                   {this.state.results.map(result => (
-                   <PetDetail
-                   id={result.id}
-                   name={result.name}
-                   src={result.photos}
-                   status={result.status}
-                   description={result.description}
-                   gender={result.gender}
-                   image={"https://dl5zpyw5k3jeb.cloudfront.net/photos/pets/" + result.id + "/1/?"}
-                   />
-                   ))}
-              </div>
-          )
+  // When this component mounts
+  componentDidMount() {
+    let currentComponent = this;
+    var key = 'igsm09LNIk7zw9Jus9SCp442VlrhlrCKzISwqh2q4jjMsgowpt';
+    var secret = '4jDSTVvRCNOvwP7gMGgr85a6FQzFl9XeBJ2sKBmJ';
+    fetch('https://api.petfinder.com/v2/oauth2/token', {
+      method: 'POST',
+      body: 'grant_type=client_credentials&client_id=' + key + '&client_secret=' + secret,
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded'
       }
+    }).then(function (resp) {
+      return resp.json();
+    }).then(function (data) {
+      console.log('token', data);
+      console.log(data.access_token)
+      const token = data.access_token;
+      //localhost is 3000 by default
+      axios.defaults.baseURL = "http://localhost:3000/";
+      axios.defaults.headers.common = { 'Authorization': `Bearer ${token}` };
+      axios.get("https://api.petfinder.com/v2/animals")
+        .then(res => {
+          console.log(res)
+          const results = res.data.animals;
+          // this.setState({ results }); //changed this reference
+          currentComponent.setState({
+            results
+          })
+        })
+        .catch(err => console.log(err));
+    }).catch(function (err) {
+      console.log('something went wrong', err);
+    });
+  }
+
+
+
+  render() {
+    return (
+      <div>
+        {this.state.results.map(result => (
+          <PetDetail
+            id={result.id}
+            name={result.name}
+            src={result.photos}
+            status={result.status}
+            description={result.description}
+            gender={result.gender}
+            image={"https://dl5zpyw5k3jeb.cloudfront.net/photos/pets/" + result.id + "/1/?"}
+          />
+        ))}
+      </div>
+    )
+  }
 
 };
 
--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,107 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import { render } from "react-dom"
+import Pet from "./Pet.js"
+import Results from "./Results";
+import { Router, Link } from "@reach/router";
+import Details from "./Details.js";
+import pf from 'petfinder-client'
+import {Provider} from "./SearchContext"
+import SearchParams from './SearchParams.js'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const petfinder = pf({
+  key: process.env.API_KEY,
+  secret: process.env.API_SECRET
+});
+
+
+class App extends React.Component {	
+
+constructor(props) {
+  super(props);
+
+  this.state = {
+    location: "Seattle, WA",
+    animal: "",
+    breed: "",
+    breeds: [],
+    handleAnimalChange: this.handleAnimalChange,
+    handleBreedChange: this.handleBreedChange,
+    handleLocationChange: this.handleLocationChange,
+    getBreeds: this.getBreeds
+  };
 }
+
+  handleLocationChange = event => {
+  this.setState({
+    location: event.target.value
+  });
+};
+
+handleAnimalChange = event => {
+  this.setState({
+    animal: event.target.value,
+    breed: ""
+  },this.getBreeds);
+};
+
+handleBreedChange = event => {
+  this.setState({
+    breed: event.target.value
+  });
+};
+
+getBreeds() {
+  if (this.state.animal) {
+    petfinder.breed
+      .list({ animal: this.state.animal })
+      .then(data => {
+        if (
+          data.petfinder &&
+          data.petfinder.breeds &&
+          Array.isArray(data.petfinder.breeds.breed)
+        ) {
+          this.setState({
+            breeds: data.petfinder.breeds.breed
+          });
+        } else {
+          this.setState({ breeds: [] });
+        }
+      })
+      .catch(console.error);
+  } else {
+    this.setState({
+      breeds: []
+    });
+  }
+}
+
+
+
+
+	  render() {
+	    return (
+	      <div>
+	        <header>
+  				<Link to="/">Adopt Me!</Link>
+  				<Link to="/search-params">
+  					<span aria-label="search" role="img">
+    					🔍
+  					</span>
+  				</Link> 
+			</header>;
+			<Provider value={this.state}>
+			<Router>
+			  <Results path="/" />
+			  <Details path="/details/:id" />
+			  <SearchParams path="/search-params" />
+			</Router>;
+			</Provider>
+	      </div>
+	    );
+	  }	    
+}
+
+render(React.createElement(App), document.getElementById("root"));
 
 export default App;
